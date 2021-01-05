a15099d33c20cec16b0111b3bbd21449a993666a
Amaan Hashmi-Ubhi <AmaanHUB@gmail.com>
2021-01-05 15:42:07 +0000

    Added some security group creation

--- main.tf
+++ main.tf
@@ -8,8 +8,64 @@ resource "aws_instance" "nodejs_app_instance" {
   ami = "ami-090c52809b287ad01"
   instance_type = "t2.micro"
   associate_public_ip_address = true
+  # associate with an already made security group with the relevant rules
+  security_groups = ["eng74-amaan-ansible"]
   tags = {
     Name = "eng74-amaan-nodeapp_terraform"
   }
   key_name = "eng74-amaan-aws"
+}
+
+resource "aws_security_group" "sg_db" {
+  name = "SG_DB_Terraform"
+  description = "a security group created by terraform, supposed to let the db communicate with the app"
+
+  ingress {
+    description = "HTTP for updates"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS for updates"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   ingress {
+     description = "For the MongoDB communication"
+     from_port = 27017
+     to_port = 27017
+     protocol = "tcp"
+     # hard coded nodejs_app_instance private ip, need to automate
+     cidr_blocks = ["172.31.24.209/32"]
+   }
+
+   egress {
+     from_port = 0
+     to_port = 0
+     protocol = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+resource "aws_instance" "mongodb_instance" {
+  ami = "ami-0ac79c29e0394a94f"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  security_groups = ["SG_DB_Terraform"]
+  tags = {
+    Name = "eng74-amaan-DB_terraform"
+  }
+  key_name = "eng74-amaan-aws"
+}
+
+output "ip" {
+  value = [aws_instance.nodejs_app_instance.*.public_ip, aws_instance.nodejs_app_instance.*.private_ip]
 }

f817363cdee00093a04a8fb3c74be3d344b2eb43
Amaan Hashmi-Ubhi <AmaanHUB@gmail.com>
2021-01-05 16:18:33 +0000

    Created some variables to add to the main file

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "key_name" {
+  default = "eng74-amaan-aws"
+}
+
+variable "nodejs_app_ami" {
+  default = "ami-090c52809b287ad01"
+}
+
+variable "mongodb_ami" {
+  default = "ami-0ac79c29e0394a94f"
+}
+
+variable "nodejs_app_SG" {
+  default = "ami-0ac79c29e0394a94f"
+}
+
+variable "mongodb_SG" {
+  default = "SG_DB_Terraform"
+}
+
+
+
+
